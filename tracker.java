5361f57c7efc41a57c69bc763530ee3315599435
maxwellio <129960841+maxyung26@users.noreply.github.com>
2024-11-14 12:47:05 -0500

    how do i use codespaces again

--- tracker.java
+++ tracker.java
@@ -1,1 +1,26 @@
+//snooze tracer (the redux), maxwell yung
+import java.util.*;
+
+public class stuff {
+    
+    public static void stuff(String[] args) {
+lines();
+    }
+    
+    
+    public static float addtracker() {
+
+    }
+    public static float checktotaltracker() {
+
+    }
+
+    public static float totalaverage() {
+
+    }
+    public static void lines() {
+        System.out.println("--------------------------------------------------");
+    }
+
+}
 
